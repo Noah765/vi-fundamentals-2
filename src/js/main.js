76ce5df76f2f75d3225fd8a3611e3f4ce5d077a6
Noah765 <99338019+Noah765@users.noreply.github.com>
2023-01-18 14:42:23 +0100

    accessibility

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,17 @@
 const buttons = document.getElementsByClassName("cta__text__button");
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].onclick = () => {
-    const collapsed = document.getElementsByClassName("contact_textbox")[0].style.height != "400px";
+    const expanded = document.getElementsByClassName("contact_textbox")[0].style.height == "400px";
 
-    document.getElementsByClassName("contact_textbox")[0].style.height = collapsed ? "400px" : 0;
-    document.getElementsByClassName("contact_textbox__textbox")[0].style.bottom = collapsed
-      ? 0
-      : "-365px";
+    for (let f = 0; f < buttons.length; f++) {
+      buttons[f].ariaExpanded = !expanded;
+    }
+
+    console.log(expanded);
+
+    document.getElementsByClassName("contact_textbox")[0].style.height = expanded ? 0 : "400px";
+    document.getElementsByClassName("contact_textbox__textbox")[0].style.bottom = expanded
+      ? "-365px"
+      : 0;
   };
 }
